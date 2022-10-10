0d44f82ae1e5d09cb89b77550c1433e4b8bcde9e
Charlotte Desvages <chdesvages@gmail.com>
2022-10-10 14:01:53 +0100

    Finished lecture - implemented first fit

--- binpacking.py
+++ binpacking.py
@@ -1,10 +1,12 @@
 # First fit heuristic for the bin packing algorithm.
 
-def first_fit(list_items, bin_capacity):
+def first_fit_decreasing(list_items, bin_capacity):
     
     # start with one empty bin
     bins = [0]
 
+    # sort the list in decreasing order
+    list_items.sort(reverse=True)
 
     # for loop over the list_items:
     for item in list_items:
@@ -12,12 +14,12 @@ def first_fit(list_items, bin_capacity):
         placed = False
 
         # for loop over the bins:
-        for b in bins:
+        for i in range(len(bins)):
             # does it fit?
             # if it fits:
-            if item <= bin_capacity - b:
+            if item <= bin_capacity - bins[i]:
                 # place item in bin
-                b += item
+                bins[i] = bins[i] + item
                 # raise the flag
                 placed = True
                 # break the loop
@@ -36,14 +38,12 @@ def first_fit(list_items, bin_capacity):
     return bins
 
 
-
-
-
 ## Testing
 list_items = [2, 1, 3, 2, 1, 2, 3, 1]
 bin_capacity = 4
 expected_bins = [4, 4, 4, 3]
 
-bins = first_fit(list_items, bin_capacity)
+bins = first_fit_decreasing(list_items, bin_capacity)
+print(bins)
 
-assert bins == expected_bins, 'Wrong result, try again!'
+assert bins == expected_bins, f'Wrong result: obtained {bins}, expected {expected_bins}'
