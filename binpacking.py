ea7f01a3fcd581318600478bb3abffec1248635b
Charlotte Desvages <chdesvages@gmail.com>
2022-10-10 13:49:33 +0100

    Finished first draft

--- binpacking.py
+++ binpacking.py
@@ -1,7 +1,39 @@
 # First fit heuristic for the bin packing algorithm.
 
 def first_fit(list_items, bin_capacity):
-    pass
+    
+    # start with one empty bin
+    bins = [0]
+
+
+    # for loop over the list_items:
+    for item in list_items:
+
+        placed = False
+
+        # for loop over the bins:
+        for b in bins:
+            # does it fit?
+            # if it fits:
+            if item <= bin_capacity - b:
+                # place item in bin
+                b += item
+                # raise the flag
+                placed = True
+                # break the loop
+                break
+
+        # if it's not placed in any bins:
+        if not placed:
+        # if placed == False:
+            bins.append(item)
+            # # open a new bin
+            # bins.append(0)
+            # # place the item in new bin
+            # bins[-1] = item
+    
+    # return list of bins
+    return bins
 
 
 
@@ -13,3 +45,5 @@ bin_capacity = 4
 expected_bins = [4, 4, 4, 3]
 
 bins = first_fit(list_items, bin_capacity)
+
+assert bins == expected_bins, 'Wrong result, try again!'
