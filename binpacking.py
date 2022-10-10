97ea7e99a1af8ddc094faeefa67df491c5506f0d
Charlotte Desvages <chdesvages@gmail.com>
2022-10-10 10:58:38 +0100

    Initial commit

---
+++ binpacking.py
@@ -0,0 +1,15 @@
+# First fit heuristic for the bin packing algorithm.
+
+def first_fit(list_items, bin_capacity):
+    pass
+
+
+
+
+
+## Testing
+list_items = [2, 1, 3, 2, 1, 2, 3, 1]
+bin_capacity = 4
+expected_bins = [4, 4, 4, 3]
+
+bins = first_fit(list_items, bin_capacity)
