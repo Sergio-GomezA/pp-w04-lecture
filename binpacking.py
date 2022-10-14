2414bd03f4957f68e947e82b17dadf4b7633bb33
Sergio-GomezA <s2441782@ed.ac.uk>
2022-10-14 09:32:51 +0100

    merge

--- binpacking.py
+++ binpacking.py
@@ -1,42 +1,5 @@
 # First fit heuristic for the bin packing algorithm.
 
-<<<<<<< HEAD
-def first_fit_decreasing(list_items, bin_capacity):
-    
-    # start with one empty bin
-    bins = [0]
-
-    # sort the list in decreasing order
-    list_items.sort(reverse=True)
-
-    # for loop over the list_items:
-    for item in list_items:
-
-        placed = False
-
-        # for loop over the bins:
-        for i in range(len(bins)):
-            # does it fit?
-            # if it fits:
-            if item <= bin_capacity - bins[i]:
-                # place item in bin
-                bins[i] = bins[i] + item
-                # raise the flag
-                placed = True
-                # break the loop
-                break
-
-        # if it's not placed in any bins:
-        if not placed:
-        # if placed == False:
-            bins.append(item)
-            # # open a new bin
-            # bins.append(0)
-            # # place the item in new bin
-            # bins[-1] = item
-    
-    # return list of bins
-=======
 def first_fit(list_items, bin_capacity):
     pass
     # Sort the list in decreasing
@@ -55,7 +18,6 @@ def first_fit(list_items, bin_capacity):
         # if we didn't find a bin with enough capacity
         if not placed:
             bins.append(item)
->>>>>>> 1d617c1 (my solution)
     return bins
 
 
@@ -64,12 +26,6 @@ list_items = [2, 1, 3, 2, 1, 2, 3, 1]
 bin_capacity = 4
 expected_bins = [4, 4, 4, 3]
 
-<<<<<<< HEAD
-bins = first_fit_decreasing(list_items, bin_capacity)
-print(bins)
-
-assert bins == expected_bins, f'Wrong result: obtained {bins}, expected {expected_bins}'
-=======
 # assert := nothing if argument true ; raises error + message if argument false
 # good for testing
 
@@ -78,4 +34,3 @@ bins = first_fit(list_items, bin_capacity)
 # assert bins == expected_bins, 'Doesn\'t match expected result'
 
 # sort them first
->>>>>>> 1d617c1 (my solution)
